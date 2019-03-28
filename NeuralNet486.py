cf7f943ba4d80cfbe13e078f2071902f369d3a2a
spencerv <spencerv@umich.edu>
2019-03-27 21:06:01 -0400

    Uploading Neural Net python and learning git

---
+++ NeuralNet486.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+from math import sqrt
+import torch.nn as nn
